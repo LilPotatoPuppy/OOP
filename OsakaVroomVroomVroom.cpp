0c64ce6abe2be3e6531a4d263c422cf439512023
Puppy <cutiepuppy707@list.ru>
2018-12-11 17:29:16 +0300

    Update OsakaVroomVroomVroom.cpp

--- OsakaVroomVroomVroom.cpp
+++ OsakaVroomVroomVroom.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-int main(){
 	class car{
 		int MaxSpeed = 200;
-		char GasolineLvl = 'Full';
+		std::string GasolineLvl = ("Full");
 		int YearOfLastChecking = 2018;
-		char SerialNumber = '2E4R';
-		void SoundsEffect(int MaxSpeed, char GasolineLvl, int YearOfLastChecking, char SerialNumber){
-			printf("Beep");
-			printf("%d %c %d %c",MaxSpeed ,GasolineLvl ,YearOfLastChecking ,SerialNumber );
+		std::string SerialNumber = ("2E4R");
+		void SoundsEffect(){
+			printf("vroom");
 		}
 	};
-}
