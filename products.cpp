dd2b7975891b56798b065926d589b8b5b009167c
Puppy <cutiepuppy707@list.ru>
2019-01-25 16:32:36 +0300

    Update products.cpp

--- products.cpp
+++ products.cpp
@@ -4,25 +4,25 @@ using namespace std;
 class Product{
 	private:
 		std::string Name;
-		int Count;
 		float Cost;
 		float Weight;
-		void Apples(){
+		public:
+		void Apples(std::string Name, float Weight){
 			cout << "Apples! Good choice! How many kilos will you take?" << endl;
 			cin >> Weight;
 			Name = ("Apples");
 		}
-		void Candies(){
+		void Candies(std::string Name, float Weight){
 			cout << "Candies! Tasty, but not healthy. \n ... \n No one actually cares. How many kilows will you take?" << endl;
 			cin >> Weight;
 			Name = ("Candies");
 		}
-		void Tomatoes(){
+		void Tomatoes(std::string Name, float Weight){
 			cout << "Tomatoes! Tasty and healhty! How many kilos will you take?" << endl;
 			cin >> Weight;
 			Name = ("Tomatoes");
 		}
-		void SetCost(){
+		void SetCost(std::string Name, float Cost, float Weight){
 			if(Name==("Apples"))Cost = Weight*73;
 			if(Name==("Candies"))Cost = Weight*56.7;
 			if(Name==("Tomatoes"))Cost = Weight*45.8;
@@ -30,18 +30,18 @@ class Product{
 		
 };
 class Buy:public Product{
-	Buy(){
+	Buy(std::string Name, int Count, float Cost, float Weight){
 		Cost=Cost;
 		Weight=Weight;
 	}
-	void SetCount(){
+	void SetCount(std::string Name, int Count, float Weight){
 			if(Name==("Apples"))Count = Weight/1.76;
 			if(Name==("Candies"))Count = Weight/0.94;
 			if(Name==("Tomatoes"))Count = Weight/2;
 		}
 };
 class Check:public Buy{
-	void Check(){
+	void Check1(){
 		cout << "You bought" << Weight << "kilos of" << Name << ". This will cost you" << Cost << "rubles. Total numbers of" << Name << "bought:" << Count << endl;
 	}
 };
