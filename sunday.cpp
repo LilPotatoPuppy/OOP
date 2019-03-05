0f7b1584923479cd7d504866a59c0c7a374a5e68
Puppy <cutiepuppy707@list.ru>
2019-03-05 16:56:37 +0300

    Update sunday.cpp

--- sunday.cpp
+++ sunday.cpp
@@ -2,23 +2,37 @@
 #include <cmath>
 int a;
 int b;
-int SignsB(int n, int b){
-	if(n==1) return b=0;
+int prevA;
+int prevB;
+int SignsB(int n, int b, int prevB){
+	if(n==1){
+		prevB=0;
+		b=1;
+		return b, prevB;
+	}
 }
-int SignsA(int n, int a){
-	if(n==1) return a=1;
+int SignsA(int n, int a, int prevA){
+	if(n==1){
+		prevA=1;
+		a=1;
+		return a, prevA;
+	}
 }
-int ReturnA(int n, int a, int b){
-	SignsA(n, a);
-	return SignsA(n-1, a) + SignsB(n-1, b);
+int ReturnA(int n, int a, int b, int prevA, int prevB){
+	prevA+=a;
+	prevB+=b;
+	a=prevA+prevB;
+	return SignsA(n-1, a, prevA) + SignsB(n-1, b, prevB);
 }
-int ReturnB(int n, int a, int b){
-	SignsB(n, b);
-	return SignsA(n-1, a) + SignsB(n-1, b) - pow(2, n-3);
+int ReturnB(int n, int a, int b, int prevA, int prevB){
+	prevB+=b;
+	prevA+=a;
+	b=prevA+prevB-pow(2, n-3);
+	return SignsA(n-1, a, prevA) + SignsB(n-1, b, prevB) - pow(2, n-3);
 }
 int Signs(int n, int a, int b){
-	ReturnB(n, a, b);
-	ReturnA(n, a, b);
+	ReturnB(n, a, b, prevA, prevB);
+	ReturnA(n, a, b, prevA, prevB);
 }
 
 int main(){
